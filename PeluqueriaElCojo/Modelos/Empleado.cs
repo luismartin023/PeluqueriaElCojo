8f02adeb54dde30b2e7b3e1e015881fa6f554284
Luis Martin Peña Mejia <luismartinpm2580@gmail.com>
2026-04-10 15:41:54 -0400

    ACTUALIZACION BREVE:
    a) SE CREO:
    
    -La clase Empleado con la interfas IComparable
    Sirve para el ranking de barberos.
    
    -La clase Producto con las interfaces IEquatable e ICloneable
    Para el inventario y detectar duplicados.

---
+++ PeluqueriaElCojo/Modelos/Empleado.cs
@@ -0,0 +1,32 @@
+﻿using PeluqueriaElCojo.Atributos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeluqueriaElCojo.Modelos
+{
+    public class Empleado : IComparable<Empleado>
+    {
+        public int Id { get; set; }
+        [Requerido]
+        public string Nombre { get; set; }
+        public string Apodo { get; set; }
+        public string Cedula { get; set; }
+        public decimal VentasAcumuladas { get; set; }
+
+        // Implementación de IComparable para ordenar por ventas
+        public int CompareTo(Empleado other)
+        {
+            if (other == null) return 1;
+            // Orden descendente (de mayor a menor venta)
+            return other.VentasAcumuladas.CompareTo(this.VentasAcumuladas);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - Ventas: RD${1:N2}", Apodo, VentasAcumuladas);
+        }
+    }
+}
---
+++ PeluqueriaElCojo/Modelos/Producto.cs
@@ -0,0 +1,31 @@
+﻿using PeluqueriaElCojo.Atributos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeluqueriaElCojo.Modelos
+{
+    public class Producto : IEquatable<Producto>, ICloneable
+    {
+        [Requerido]
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+        // IEquatable: Detectar duplicados por código
+        public bool Equals(Producto other)
+        {
+            if (other == null) return false;
+            return this.Codigo == other.Codigo;
+        }
+
+        // ICloneable: Duplicar un registro
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
